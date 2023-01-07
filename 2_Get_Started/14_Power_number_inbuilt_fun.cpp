debdbfb1e93a43630d9a0835243d301556a0b7ec
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-01-08 02:35:23 +0530

    Add files via upload

---
+++ 2_Get_Started/14_Power_number_inbuilt_fun.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    double base,exp;
+    cout<<"Enter the base number:";
+    cin>>base;
+    cout<<"Ente the exponential:";
+    cin>>exp;
+    cout<<pow(base,exp);
+}
