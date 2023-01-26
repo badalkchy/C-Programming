fcdf9b3c5dc820d2f04e9839d1693b4d8ee1e04a
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-01-26 10:03:34 -0800

    Delete 2Even_odd.cpp

--- 2Even_odd.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b,c;
-    cout<<"Enter the number:";
-    cin>>b;
-    b%2==0? cout<<"Even":cout<<"Odd"; /*Ternary operator syntax*/
-}
