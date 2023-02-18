2119604c71c79da392e57a3f2790aa398d12afa4
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-18 22:01:24 +0530

    Add files via upload

---
+++ Code_for_win/01_Basic_Exersice/01_Input_Output.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number :";   // cout --> keywork use to show output 
+    cin>>n;                      //  cin  --> use to take input any data type
+    cout<<n<<" is output";
+}
