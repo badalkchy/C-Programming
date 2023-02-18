2b7e4d9ae970f4eee47df9406ef1920ddcbf4ef1
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-18 22:02:26 +0530

    Add files via upload

---
+++ Code_for_win/01_Basic_Exersice/05_Find_rectangle_parameter.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float l,w;
+    std::cout.setf(std::ios_base::fixed,std::ios_base::floatfield);
+    std::cout.precision(2);
+
+    cout<<"Enter the lenght : ";
+    cin>>l;
+    cout<<"Enter the width :";
+    cin>>w;
+    cout<<(l*2)+(w*2);
+}
