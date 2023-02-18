36465a737196f2d853d9541938ec7649384f13ac
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-18 22:01:34 +0530

    Add files via upload

---
+++ Code_for_win/01_Basic_Exersice/02_Control_decimal_after_point.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float n;
+    std::cout.setf(std::ios_base::fixed,std::ios_base::floatfield);
+    std::cout.precision(2);
+    
+    cout<<"Enter the number :";
+    cin>>n;
+    cout<<n;
+}
