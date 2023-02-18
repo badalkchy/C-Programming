aa262edecf9a0205aaa723ea83a239d0951de35d
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-18 22:01:55 +0530

    Add files via upload

---
+++ Code_for_win/01_Basic_Exersice/03_Sum_of_two.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float n,m;
+    std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+    std::cout.precision(2);
+
+    cout<<"Enter the first number :";
+    cin>>n;
+    cout<<"Enter the second number :";
+    cin>>m;
+    cout<<m+n;
+}
