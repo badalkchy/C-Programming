9ef89b9e8bdce5ea6386d5e1d966506308fd170a
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-19 22:36:13 +0530

    Add files via upload

---
+++ Code_for_win/01_Basic_Exersice/06_Find_the_area_of_rectangle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float l,w;
+    std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+    std::cout.precision(2);
+
+    cout<<"Enter the first number :";
+    cin>>l;
+    cout<<"Enter the second number :";
+    cin>>w;
+    cout<<"Area of rectangle is "<<(l*w);
+}
