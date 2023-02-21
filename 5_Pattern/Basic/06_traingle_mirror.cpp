40122cca173cf380d9c0e601b7a08f5536474c82
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-22 03:16:16 +0530

    Add files via upload

---
+++ 5_Pattern/Basic/06_traingle_mirror.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the number:";
+    cin>>b;
+    for(int a=1;a<=b;a++){
+        for(int j=1;j<=b;j++){
+            if(j<=b-a){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
