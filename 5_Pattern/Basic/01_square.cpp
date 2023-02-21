2f29b24152e94075fc81b355dfb986ae5f04a14a
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-21 22:10:32 +0530

    Add files via upload

---
+++ 5_Pattern/Basic/01_square.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int b,a;
+    cout<<"Enter the number:";
+    cin>>b;
+    for(int a=0;a<b;a++){
+        for(int j=0;j<b;j++){
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+}
