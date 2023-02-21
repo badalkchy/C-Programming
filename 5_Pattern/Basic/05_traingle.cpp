a13b4d4a8b5dbb6e1dc58c44da4f47ec56d5d135
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-21 22:11:50 +0530

    Add files via upload

---
+++ 5_Pattern/Basic/05_traingle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the number:";
+    cin>>b;
+    for(int a=1;a<=b;a++){
+        for(int j=1;j<=a;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
