7c5bfa1aa67daa8cd66fd0a8918473531cb4995f
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-23 21:35:55 +0530

    Add files via upload

---
+++ 5_Pattern/Basic/14_pyramid_center_star.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b;
+    cout<<"Enter the number:";
+    cin>>b;
+    for(int a=1;a<=b;a++){
+        for(int j=1;j<=b-a;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=a*2-1;k++){
+            if(k==1||k==a*2-1||a==1||a==b||k==a){
+            cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
