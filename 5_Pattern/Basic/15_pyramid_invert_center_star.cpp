895d2c94327c33e5808a4b00e85409de7c864074
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-23 21:36:56 +0530

    Add files via upload

---
+++ 5_Pattern/Basic/15_pyramid_invert_center_star.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b;
+    cout<<"Enter the number:";
+    cin>>b;
+    for(int a=1;a<=b;a++){
+        for(int j=1;j<=a-1;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=(b*2)-(a*2-1);k++){
+            if(k==1||k==(b*2)-(a*2-1)||a==1||a==b||k==b-a+1){
+            cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
